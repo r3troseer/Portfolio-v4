266985ea0d64120597712edbed94f2fc2836d6f8
Pius Agboola <piagbo3d@gmail.com>
2026-07-30 20:15:41 +0100

    fix(api): route telemetry logs to stdout

--- apps/api/config/settings.py
+++ apps/api/config/settings.py
@@ -222,6 +222,7 @@ LOGGING = {
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
         },
     },
     "loggers": {
